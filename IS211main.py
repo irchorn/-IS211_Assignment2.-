7843608c22439899d98f945da67eb6149a03cca2
irynach <70407458+irchorn@users.noreply.github.com>
2021-02-10 20:18:15 -0500

    assignment 2

---
+++ IS211main.py
@@ -0,0 +1,26 @@
+
+
+
+def downloadData(url):
+    return url
+
+
+import urllib.request
+
+with urllib.request.urlopen('http://python.org/') as response:
+    html = response.read()
+
+
+def processData(file):
+    for line in file:
+        return person_bd
+    person_bd = {ID: (name, birthday)}
+
+
+import datetime
+
+birthday = datetime.date.strptime('%m/%d/%Y').date()
+print(birthday)
+
+
+
